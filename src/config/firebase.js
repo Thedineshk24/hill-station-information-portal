9731f96c5729c70a823b0a5cb81ea32347ba0255
Dinesh Katariya <33573799+Thedineshk24@users.noreply.github.com>
2021-01-17 16:06:39 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,4 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export default {
-    apiKey: "AIzaSyABEMrNft0qSoOvBFdr1MRlPH_DZO9kLVQ",
-    authDomain: "hillstation-919a0.firebaseapp.com",
-    projectId: "hillstation-919a0",
-    storageBucket: "hillstation-919a0.appspot.com",
-    messagingSenderId: "544657170616",
-    appId: "1:544657170616:web:96c3ce5b08f343e5fb144d",
-    measurementId: "G-VCM38908C1"
+// firebase config
   };
